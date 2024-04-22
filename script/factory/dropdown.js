7eb020cc188b93ef8fa231ac99949be7dd3e9106
Anaïs Leclerc <anais-leclerc@hotmail.fr>
2024-04-22 13:38:34 +0200

    feat: search functions

--- script/factory/dropdown.js
+++ script/factory/dropdown.js
@@ -1,22 +1,28 @@
+import { filter } from "../script.js";
+import { getRecipe } from "../script.js";
+
+/**
+ * Class representing a factory for creating list items.
+ */
 class ListItemFactory {
   /**
-   * Creates a list item (<li>) with the specified text.
-   * @param {string} text - The text to display in the list item.
-   * @returns {HTMLLIElement} - The created list item.
+   * Create a list item.
+   * @param {string} text - The text content of the list item.
+   * @return {HTMLElement} The created list item.
    */
   createListItem(text) {
     const li = document.createElement('li');
-    li.textContent = text.charAt(0).toUpperCase() + text.slice(1); // Capitalize the first letter
+    li.textContent = text.charAt(0).toUpperCase() + text.slice(1);
     li.classList.add('dropdown-item');
     return li;
   }
 }
 
 /**
- * Populates HTML dropdowns with data retrieved from a JSON endpoint.
- * @param {string} jsonEndpoint - The URL of the JSON endpoint to retrieve data from.
+ * Populate dropdowns with unique sorted ingredients, appliances, and utensils from recipes.
+ * @param {Array} recipes - The recipes to extract data from.
  */
-async function populateDropdowns(jsonEndpoint) {
+export async function populateDropdowns(recipes) {
   const dropdowns = {
     ingredients: document.querySelector('.dropdown-ingredients ul'),
     appareils: document.querySelector('.dropdown-appareils ul'),
@@ -26,63 +32,62 @@ async function populateDropdowns(jsonEndpoint) {
   const factory = new ListItemFactory();
 
   try {
-    const response = await fetch(jsonEndpoint);
-    const data = await response.json();
+    // Clear the dropdowns
+    dropdowns.ingredients.innerHTML = '';
+    dropdowns.appareils.innerHTML = '';
+    dropdowns.ustensiles.innerHTML = '';
 
-    // Check if the recipes array exists in your JSON object
-    if (data.recipe && Array.isArray(data.recipe)) {
-      const uniqueIngredients = new Set();
-      const uniqueAppareils = new Set();
-      const uniqueUstensiles = new Set();
+    const uniqueIngredients = new Set();
+    const uniqueAppareils = new Set();
+    const uniqueUstensiles = new Set();
 
-      // Helper function to normalize words and remove common suffixes
-      const normalizeWord = word => {
-        return word.toLowerCase().replace(/s$/, ''); // Remove "s" at the end
-      };
-
-      // Populate sets with unique values
-      data.recipe.forEach(recipe => {
+    recipes.forEach(recipe => {
+      if (recipe.ingredients) {
         recipe.ingredients.forEach(ingredient => {
-          const normalizedIngredient = normalizeWord(ingredient.ingredient);
-          uniqueIngredients.add(normalizedIngredient);
+          uniqueIngredients.add(ingredient.ingredient.toLowerCase());
         });
-        
-        const normalizedAppareil = normalizeWord(recipe.appliance);
-        uniqueAppareils.add(normalizedAppareil);
-
-        recipe.ustensils.forEach(ustensil => {
-          const normalizedUstensil = normalizeWord(ustensil);
-          uniqueUstensiles.add(normalizedUstensil);
+      }
+      
+      uniqueAppareils.add(recipe.appliance.toLowerCase());
+    
+      if (recipe.ustensiles) {
+        recipe.ustensiles.forEach(ustensil => {
+          uniqueUstensiles.add(ustensil.toLowerCase());
         });
-      });
+      }
+    });    
 
-      // Convert sets to arrays and sort them alphabetically
-      const sortedIngredients = Array.from(uniqueIngredients).sort();
-      const sortedAppareils = Array.from(uniqueAppareils).sort();
-      const sortedUstensiles = Array.from(uniqueUstensiles).sort();
+    const sortedIngredients = Array.from(uniqueIngredients).sort();
+    const sortedAppareils = Array.from(uniqueAppareils).sort();
+    const sortedUstensiles = Array.from(uniqueUstensiles).sort();
 
-      // Populate dropdowns with sorted data
-      sortedIngredients.forEach(item => {
-        const li = factory.createListItem(item);
-        dropdowns.ingredients.appendChild(li);
-      });
+    sortedIngredients.forEach(item => {
+      const li = factory.createListItem(item);
+      li.addEventListener('click', filter);
+      dropdowns.ingredients.appendChild(li);
+    });
 
-      sortedAppareils.forEach(item => {
-        const li = factory.createListItem(item);
-        dropdowns.appareils.appendChild(li);
-      });
+    sortedAppareils.forEach(item => {
+      const li = factory.createListItem(item);
+      li.addEventListener('click', filter);
+      dropdowns.appareils.appendChild(li);
+    });
 
-      sortedUstensiles.forEach(item => {
-        const li = factory.createListItem(item);
-        dropdowns.ustensiles.appendChild(li);
-      });
-    } else {
-      console.error('The "recipe" array is missing in the JSON file.');
-    }
+    sortedUstensiles.forEach(item => {
+      const li = factory.createListItem(item);
+      li.addEventListener('click', filter);
+      dropdowns.ustensiles.appendChild(li);
+    });
+
   } catch (error) {
     console.error('Error fetching data:', error);
   }
 }
 
-// Call the function to populate dropdowns with JSON data
-populateDropdowns('././data/recipes.json');
+/**
+ * IIFE to populate dropdowns with recipes data.
+ */
+(async () => {
+  const recipes = await getRecipe();
+  populateDropdowns(recipes);
+})();
--- script/templates/recipe.js
+++ script/templates/recipe.js
@@ -1,9 +1,19 @@
+/**
+ * Creates a recipe template from the provided data.
+ *
+ * @param {Object} data - The recipe data.
+ * @returns {Object} An object containing the recipe's DOM elements and other details.
+ */
 export function recipeTemplate(data) {
-    const { body, title, recipe, txt, ingredients, divIngredients, divDetails, image, name, description, } = data;
+    const { body, title, recipe, txt, ingredients, appliance, ustensil, divIngredients, divDetails, image, name, description, } = data;
 
     const picture = `assets/img/${image}`;
 
-    // Generates DOM of the recipe
+    /**
+     * Generates the DOM of the recipe.
+     *
+     * @returns {HTMLElement} The created recipe card.
+     */
     function getRecipeDOM() {
         // Create recipe card
         const card = document.createElement('a');
@@ -11,68 +21,92 @@ export function recipeTemplate(data) {
         card.setAttribute('href', '#');
         card.setAttribute('tabindex', 0);
         card.setAttribute('role', 'section');
-        card.setAttribute('aria-labelledby', 'titre de la recette');
+        card.setAttribute('aria-labelledby', 'recipe title');
         card.setAttribute('style', 'width: 23.75rem');
         card.focus();
-    
+
+        // Create picture element
         const pictureElement = document.createElement('img');
         pictureElement.classList.add('card-img-top');
         pictureElement.setAttribute('src', picture);
-        pictureElement.setAttribute('alt', 'Illustration de la recette');
+        pictureElement.setAttribute('alt', 'Recipe illustration');
         pictureElement.setAttribute('style', 'height: 15.813rem');
-    
+
+        // Create body element
         const bodyElement = document.createElement('div');
         bodyElement.classList.add('card-body');
-    
+
+        // Create title element
         const titleElement = document.createElement('h2');
         titleElement.classList.add('card-title');
-        titleElement.setAttribute('id', 'titre de la recette');
+        titleElement.setAttribute('id', 'recipe title');
         titleElement.setAttribute('aria-label', name);
         titleElement.textContent = name;
-    
+
+        // Create recipe title element
         const recipeTitleElement = document.createElement('h3');
-        recipeTitleElement.textContent = 'RECETTE';
+        recipeTitleElement.textContent = 'RECIPE';
         recipeTitleElement.classList.add('card-subtitle');
-    
+
+        // Create text element
         const txtElement = document.createElement('p');
         txtElement.textContent = description;
         txtElement.classList.add('card-text');
-    
+
+        // Create ingredients div
         const ingredientsDiv = document.createElement('div');
         ingredientsDiv.classList.add('card-ingredients');
-    
+
+        // Create each ingredient item
         data.ingredients.forEach(ingredientData => {
             const ingredientItem = document.createElement('p');
             ingredientItem.classList.add('card-text-ingredient');
             ingredientItem.textContent = ingredientData.ingredient;
-            
+
             const quantityUnitItem = document.createElement('p');
             quantityUnitItem.classList.add('card-text-qty');
             quantityUnitItem.textContent = `${ingredientData.quantity || ''} ${ingredientData.unit || ''}`;
-    
+
             const ingredientDetailsDiv = document.createElement('div');
             ingredientDetailsDiv.classList.add('card-ingredients-details');
             ingredientDetailsDiv.appendChild(ingredientItem);
             ingredientDetailsDiv.appendChild(quantityUnitItem);
-    
+
             ingredientsDiv.appendChild(ingredientDetailsDiv);
         });
-    
+
+        // Create ingredients title element
         const ingredientsTitleElement = document.createElement('h3');
-        ingredientsTitleElement.textContent = 'INGRÉDIENTS';
+        ingredientsTitleElement.textContent = 'INGREDIENTS';
         ingredientsTitleElement.classList.add('card-subtitle');
-    
+
+        // Create appliance element
+        const applianceElement = document.createElement('p');
+        applianceElement.textContent = appliance;
+        applianceElement.classList.add('appliance');
+        applianceElement.style.display = "none"; //hidden for filter purpose
+
+        // Create ustensil element
+        const ustensilElement = document.createElement('p');
+        ustensilElement.textContent = ustensil;
+        ustensilElement.classList.add('ustensil');
+        ustensilElement.style.display = "none"; //hidden for filter purpose
+
+        // Append elements to body element
         bodyElement.appendChild(titleElement);
         bodyElement.appendChild(recipeTitleElement);
         bodyElement.appendChild(txtElement);
         bodyElement.appendChild(ingredientsTitleElement);
         bodyElement.appendChild(ingredientsDiv);
-    
+        bodyElement.appendChild(applianceElement);
+        bodyElement.appendChild(ustensilElement);
+
+        // Append elements to card
         card.appendChild(pictureElement);
         card.appendChild(bodyElement);
-    
-        return card;
-    }      
 
-    return { picture, body, title, recipe, txt, ingredients, divIngredients, divDetails, getRecipeDOM };
+        return card;
+    }
+
+    return { picture, body, title, recipe, txt, ingredients, appliance, ustensil, divIngredients, divDetails, getRecipeDOM };
 }
